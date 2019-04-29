c0a64651e3746f0ef41727c280b23a1b670b000b
wmc7n <44489337+wmc7n@users.noreply.github.com>
2019-04-29 16:42:02 +0900

    Create bit52.js

---
+++ bit52.js
@@ -0,0 +1,89 @@
+module.exports.and52 = function(v1, v2) {
+    let hi = 0x80000000;
+    let low = 0x7fffffff;
+    let hi1 = ~~(v1 / hi);
+    let hi2 = ~~(v2 / hi);
+    let low1 = v1 & low;
+    let low2 = v2 & low;
+    let h = hi1 & hi2;
+    let l = low1 & low2;
+    return h*hi + l;
+}
+
+module.exports.or52 = function(v1, v2) {
+    let hi = 0x80000000;
+    let low = 0x7fffffff;
+    let hi1 = ~~(v1 / hi);
+    let hi2 = ~~(v2 / hi);
+    let low1 = v1 & low;
+    let low2 = v2 & low;
+    let h = hi1 | hi2;
+    let l = low1 | low2;
+    return h*hi + l;
+}
+
+module.exports.ls52 = function(num, bits) {
+    return num * Math.pow(2,bits);
+}
+
+module.exports.rs52 = function(num, bits) {
+    return ~~(num / Math.pow(2,bits));
+}
+
+module.exports.frexp = function(arg) {
+  arg = Number(arg);
+  const result = [arg, 0];
+  if (arg !== 0 && Number.isFinite(arg)) {
+    const absArg = Math.abs(arg);
+    const log2 = Math.log2 || function log2 (n) { return Math.log(n) * Math.LOG2E }
+    let exp = Math.max(-1023, ~~(log2(absArg)) + 1);
+    let x = absArg * Math.pow(2, -exp);
+    while (x < 0.5) {
+      x *= 2;
+      exp--;
+    }
+    while (x >= 1) {
+      x *= 0.5;
+      exp++;
+    }
+    if (arg < 0) {
+      x = -x;
+    }
+    result[0] = x;
+    result[1] = exp;
+  }
+  return result;
+}
+
+module.exports.fmod = function(a, b) {
+  return Number((a - (~~(a / b) * b)).toPrecision(8));
+}
+
+module.exports.to_ary = function(num, bits) {
+	bits = bits === undefined
+    ? Math.max(1, frexp(num)[1])
+    : bits;
+	let t = [];
+  for (var i = bits - 1; bits >= 0; bits--) {
+		t[b] = fmod(num, 2);
+		num = ~~((num - t[b]) / 2);
+  }
+	return t;
+}
+
+module.exports.to_num = function(bits) {
+	let t = 0;
+  for (var i = 0; i < bits.length; i++) {
+		t += bits[i]*Math.pow(2, bits.length-1-i);
+  }
+	return t;
+}
+
+module.exports.reverse = function(num, bits) {
+	const ary = to_bits(num, bits);
+	let tmp = [];
+  for (var i = 0; i < ary.length; i++) {
+    tmp.push(ary[ary.length-i-1]);
+  }
+	return to_num(tmp);
+}
